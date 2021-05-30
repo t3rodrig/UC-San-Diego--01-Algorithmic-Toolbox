31ab9bd2e5f2d7c0758ac8a931556951154c93a3
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2021-05-30 16:26:38 -0500

    Do problem w2-p2

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -13,6 +13,19 @@ def get_fibonacci_last_digit_naive(n):
     return current % 10
 
 
+def get_fibonacci_last_digit_efficient(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % 10
+
+    return current % 10
+
+
 if __name__ == '__main__':
     n = int(input())
-    print(get_fibonacci_last_digit_naive(n))
+    print(get_fibonacci_last_digit_efficient(n))
