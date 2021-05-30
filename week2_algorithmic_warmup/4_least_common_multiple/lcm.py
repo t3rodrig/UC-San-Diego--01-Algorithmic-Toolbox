36ad060f4a5d6f725cc24c04bb2bd1b391c90c1b
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2021-05-30 18:03:05 -0500

    Do problem 2.4

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,5 +1,14 @@
 # Uses python3
 
+def gcd_euclid(a, b):
+    if b == 0:
+        return a
+
+    a %= b
+
+    return gcd_euclid(b, a)
+
+
 def lcm_naive(a, b):
     for l in range(1, a*b + 1):
         if l % a == 0 and l % b == 0:
@@ -8,6 +17,11 @@ def lcm_naive(a, b):
     return a*b
 
 
+def lcm_efficient(a, b):
+    number = int((a * b) / gcd_euclid(a, b))
+    return number
+
+
 if __name__ == '__main__':
     a, b = map(int, input().split())
-    print(lcm_naive(a, b))
+    print(lcm_efficient(a, b))
