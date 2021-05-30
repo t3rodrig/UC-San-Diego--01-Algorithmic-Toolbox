2336f961834921052321b3e97497535c82a414f0
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2021-05-30 16:33:47 -0500

    Refactor starter code
    week2 problem3

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,5 +1,4 @@
 # Uses python3
-import sys
 
 def gcd_naive(a, b):
     current_gcd = 1
@@ -10,7 +9,7 @@ def gcd_naive(a, b):
 
     return current_gcd
 
+
 if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
+    a, b = map(int, input().split())
     print(gcd_naive(a, b))
