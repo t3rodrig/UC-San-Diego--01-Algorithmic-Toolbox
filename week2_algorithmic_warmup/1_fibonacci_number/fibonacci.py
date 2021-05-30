97b769286224178f7368ef6b65981f6c8ce28f5a
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2021-05-30 15:53:05 -0500

    Do problem w2-p1

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -6,6 +6,14 @@ def calc_fib(n):
     return calc_fib(n - 1) + calc_fib(n - 2)
 
 
+def calc_fib_fast(n):
+    numbers = [0, 1]
+    for i in range(2, n+1):
+        num = numbers[i-1] + numbers[i-2]
+        numbers.append(num)
+    return numbers[n]
+
+
 if __name__ == '__main__':
     n = int(input())
-    print(calc_fib(n))
+    print(calc_fib_fast(n))
