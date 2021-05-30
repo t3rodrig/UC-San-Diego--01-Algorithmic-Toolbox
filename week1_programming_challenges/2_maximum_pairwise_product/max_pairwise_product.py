e90453b1f8f7447967b7251b175114f1b2cf9861
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2021-05-30 12:11:46 -0500

    Do week1

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -11,8 +11,21 @@ def max_pairwise_product(numbers):
 
     return max_product
 
+def max_pairwise_product_fast(numbers):
+    n = len(numbers)
+    index = 0
+    for i in range(1, n):
+        if numbers[i] > numbers[index]:
+            index = i
+    numbers[index], numbers[-1] = numbers[-1], numbers[index]
+    index = 0
+    for i in range(1, n - 1):
+        if numbers[i] > numbers[index]:
+            index = i
+    numbers[index], numbers[-2] = numbers[-2], numbers[index]
+    return numbers[-1] * numbers[-2]
 
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product_fast(input_numbers))
